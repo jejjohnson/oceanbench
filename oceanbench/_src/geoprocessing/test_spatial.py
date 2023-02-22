1688fa3ed2622c94e28e7f14a174305b38a2ac96
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-02-22 15:34:32 +0100

    updated tests. still pass. updated pytest.ini updated environment files. (xemsf works for macos m1)

--- oceanbench/_src/geoprocessing/test_spatial.py
+++ oceanbench/_src/geoprocessing/test_spatial.py
@@ -4,7 +4,6 @@ from .spatial import transform_180_to_360, transform_360_to_180
 
 
 def test_transform_180_to_360_bounds():
-
     coords = np.linspace(-180, 180, 20)
 
     new_coords = transform_180_to_360(coords)
@@ -12,21 +11,21 @@ def test_transform_180_to_360_bounds():
     assert new_coords.min() >= 0.0
     assert new_coords.max() <= 360
 
-    new_coords = transform_180_to_360(coords)
+    new_coords = transform_180_to_360(new_coords)
 
     assert new_coords.min() >= 0.0
     assert new_coords.max() <= 360
 
-def test_transform_360_to_180_bounds():
 
+def test_transform_360_to_180_bounds():
     coords = np.linspace(0, 360, 20)
 
     new_coords = transform_360_to_180(coords)
 
-    assert new_coords.min() >=-180
+    assert new_coords.min() >= -180
     assert new_coords.max() <= 180
 
-    new_coords = transform_360_to_180(coords)
+    new_coords = transform_360_to_180(new_coords)
 
     assert new_coords.min() >= -180
     assert new_coords.max() <= 180
