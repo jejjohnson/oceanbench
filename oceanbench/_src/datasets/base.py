41411c4bc21277d1ed0e1f4bf2cf5d2a6e850cc2
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-03-29 19:39:28 +0200

    Working xrdataset batcher.
    - refactored it to a dataclass (removed pytorch dependency)
    - tested it (in notebook) for 1D TS, 2D spatial, 2D+T
    - also works for latents
    - added a new helper for roots for notebooks.

--- oceanbench/_src/datasets/base.py
+++ oceanbench/_src/datasets/base.py
@@ -1,4 +1,5 @@
 import typing as tp
+from dataclasses import dataclass
 import itertools
 import numpy as np
 import xarray as xr
@@ -13,8 +14,148 @@ from oceanbench._src.datasets.utils import (
     update_dict_xdims,
     check_lists_subset,
     get_patches_size,
-    get_slices
+    get_slices,
+    reconstruct_from_items,
+    list_product
 )
+
+
+@dataclass
+class XRDABatcher:
+    da: xr.DataArray
+    patches: tp.Dict[str, int]
+    strides: tp.Dict[str, int]
+    da_dims: tp.Dict[str, int]
+    da_size: tp.Dict[str, int]
+    return_coords: bool = False
+    
+    def __init__(
+            self, 
+            da: xr.DataArray,
+            patches: tp.Optional[tp.Dict[str, int]]=None,
+            strides: tp.Optional[tp.Dict[str, int]]=None,
+            domain_limits: tp.Optional[tp.Dict]=None,
+            check_full_scan: bool=False,
+        ):
+        
+        if domain_limits is not None:
+            da_dims = get_dims_xrda(da)
+            check_lists_subset(list(domain_limits.keys()), list(da_dims.keys()))
+            da = da.sel(**domain_limits)
+        
+        self.da = da
+        self.da_dims = get_dims_xrda(da)
+        
+        self.da_size, self.patches, self.strides = get_patches_size(
+            dims=self.da_dims,
+            patches=patches if patches is not None else {},
+            strides=strides if strides is not None else {},
+        )
+        if check_full_scan:
+            for dim in self.patches:
+                if (self.da_dims[dim] - self.patches[dim]) % self.strides[dim] != 0:
+                    msg = f"\nIncomplete scan in dimension dim {dim}:"
+                    msg += f"\nDataArray shape on this dim {self.da_dims[dim]} "
+                    msg += f"\nPatch_size along this dim {self.patches[dim]} "
+                    msg += f"\nStride along this dim {self.strides[dim]} "
+                    msg += f"\n[shape - patch_size] should be divisible by stride: "
+                    msg += f"{(self.da_dims[dim] - self.patches[dim]) % self.strides[dim]}"
+                    raise IncompleteScanConfiguration(msg)
+        
+    def __repr__(self) -> str:
+        msg = "XArray Patcher"
+        msg += "\n=============="
+        msg += f"\nDataArray Size: {self.da_dims}"
+        msg += f"\nPatches:        {self.patches}"
+        msg += f"\nStrides:        {self.strides}"
+        msg += f"\nNum Batches:    {self.da_size}"
+        return msg
+    
+    def __str__(self) -> str:
+        msg = "XArray Patcher"
+        msg += "\n=============="
+        msg += f"\nDataArray size: {self.da_dims}"
+        msg += f"\nPatches:        {self.patches}"
+        msg += f"\nStrides:        {self.strides}"
+        msg += f"\nNum Batches:    {self.da_size}"
+        return msg
+    
+    @property
+    def coord_names(self) -> tp.List[str]:
+        return list(self.da_dims.keys())
+    def get_da_slice(self, idx: int, da: xr.DataArray) -> xr.DataArray:
+        
+        slices = get_slices(idx, 
+                          da_size=self.da_size, 
+                          patches=self.patches, 
+                          strides=self.strides
+                         )
+        return self.da.isel(**slices)
+    
+    def __len__(self):
+        return list_product(list(self.da_size.values()))
+    
+    
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+    
+    def __getitem__(self, item):
+        output = {}
+
+        slices = get_slices(
+            idx=item, 
+            da_size=self.da_size, 
+            patches=self.patches, 
+            strides=self.strides
+        )
+        
+        item = self.da.isel(**slices)
+        
+        item = item.transpose(*self.coord_names)
+        
+        if self.return_coords:
+            return item.coords.to_dataset()
+                    
+        return item.data.astype(np.float32)
+    
+    def reconstruct(
+        self, 
+        batches: tp.List[np.ndarray], 
+        dims_label: tp.List[str], 
+        weight: tp.Optional[np.ndarray]=None
+    ) -> xr.DataArray:
+        """
+        takes as input a list of np.ndarray of dimensions (b, *, *patch_dims)
+        return a stitched xarray.DataArray with the coords of patch_dims
+    batches: list of torch tensor correspondin to batches without shuffle
+        weight: tensor of size patch_dims corresponding to the weight of a prediction depending on the position on the patch (default to ones everywhere)
+        overlapping patches will be averaged with weighting
+        """
+
+        items = list(itertools.chain(*batches))
+        rec_da = reconstruct_from_items(
+            items=items,
+            dims_label=dims_label,
+            torch_xrds=self,
+            weight=weight
+        ) 
+        
+        rec_da.attrs = self.da.attrs
+        
+        return rec_da
+    
+    def get_coords(self) -> tp.List[xr.DataArray]:
+        self.return_coords = True
+        coords = []
+        try:
+            for i in range(len(self)):
+                coords.append(self[i])
+        finally:
+            self.return_coords = False
+            return coords
+        
+        
 
 
 class XArrayDataset(torch.utils.data.Dataset):
@@ -38,7 +179,7 @@ class XArrayDataset(torch.utils.data.Dataset):
             domain_limits: tp.Optional[tp.Dict]=None,
             check_full_scan: bool=False,
             check_dim_order: bool=False,
-            transforms: tp.Optional[tp.Callable]=None
+            transforms: tp.Optional[tp.Callable]=None,
     ):
         """
         Args:
@@ -65,7 +206,8 @@ class XArrayDataset(torch.utils.data.Dataset):
                 (defaults to one stride per dimension)
             domain_limits (Dict): dict of da dimension to slices of domain
                 to select for patch extractions
-            ds_size (Dict): the dictionary of dimensions
+            ds_size (Dict): the dictionary of dimensions for the slicing
+            da_dims (Dict): the dictionary of the original dimensions
         """
         super().__init__()
         self.return_coords = False
@@ -76,12 +218,12 @@ class XArrayDataset(torch.utils.data.Dataset):
             check_lists_subset(list(domain_limits.keys()), list(da_dims.keys()))
             da = da.sel(**domain_limits)
         
-        da_dims = get_dims_xrda(da)
+        self.da_dims = get_dims_xrda(da)
         
         self.da = da
 
         self.da_size, self.patches, self.strides = get_patches_size(
-            dims=da_dims, 
+            dims=self.da_dims, 
             patches=patches if patches is not None else {}, 
             strides=strides if strides is not None else {}
             )
@@ -131,20 +273,26 @@ class XArrayDataset(torch.utils.data.Dataset):
             return coords
 
     def __getitem__(self, item):
+        
+        output = {}
 
         slices = get_slices(
-            idx=item, da_size=self.da_size, patches=self.patches, strides=self.strides
+            idx=item, 
+            da_size=self.da_size, 
+            patches=self.patches, 
+            strides=self.strides
         )
         
         item = self.da.isel(**slices)
-
+        
         if self.return_coords:
-            return item.coords.to_dataset()[list(self.patches)]
-
+            return item.coords.to_dataset()
+            
         item = item.data.astype(np.float32)
         if self.transforms is not None:
             return self.transforms(item)
         return item
+    
 
     def reconstruct(self, batches, weight=None):
         """
--- oceanbench/_src/datasets/utils.py
+++ oceanbench/_src/datasets/utils.py
@@ -1,7 +1,43 @@
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, NamedTuple, Optional, Iterable
+from collections import OrderedDict
 import xarray as xr
 import numpy as np
 from functools import reduce
+from tqdm import tqdm
+# from oceanbench._src.datasets.base import XArrayDataset
+
+
+# class DAPatchParams(NamedTuple):
+#     patches: Dict[str, int]
+#     strides: Dict[str, int]
+#     da_dims: Dict[str, int]
+#     da_size: Dict[str, int]
+    
+#     def __init__(self, 
+#                  da: xr.DataArray,
+#                  patches: Optional[Dict[str, int]],
+#                  strides: Optional[Dict[str, int]],
+#                 ):
+        
+#         self.da_dims = get_dims_xrda(da)
+        
+#         self.da_size, self.patches, self.strides = get_patches_size(
+#             dims=self.da_dims,
+#             patches=patches if patches is not None else {},
+#             strides=strides if strides is not None else {},
+#         )
+    
+#     def get_da_slice(self, idx, da: xr.DataArray) -> xr.DataArray:
+        
+#         slices = get_slices(idx, 
+#                           da_size=self.da_size, 
+#                           patches=self.patches, 
+#                           strides=self.strides
+#                          )
+#         return da.isel(**slices)
+    
+#     def num_items(self):
+#         return list_product(self.da_size.values)
 
 
 def check_lists_equal(list_1: List, list_2: List):
@@ -15,7 +51,7 @@ def check_lists_subset(list_1: List, list_2: List):
     
 
 def get_dims_xrda(da: xr.DataArray) -> Dict[str, int]:
-    return dict(zip(da.dims, da.shape))
+    return OrderedDict(zip(da.dims, da.shape))
 
 
 def update_dict_xdims(da: xr.DataArray, dims: Dict={}, default_size: bool=True) -> Dict:
@@ -51,12 +87,28 @@ def update_dict_keys(source: Dict[str, int], new: Dict[str, int], default: bool=
         >>> update
         {"x": 100, "y": 1}
     """
-    if default:
-        update = {f"{ikey}": source[ikey] for ikey in source.keys() if ikey not in list(new.keys())}
-    else:
-        update = {f"{ikey}": 1 for ikey in source.keys() if ikey not in list(new.keys())}
+#     if default:
+#         update = OrderedDict()
+        
+#         for ikey in source.keys():
+#             if ikey not in new.keys():
+#                 update[ikey] = source[ikey]
+#             else:
+#                 update[ikey] = new[ikey]
+                
+#         # update = {f"{ikey}": source[ikey] for ikey in source.keys() if ikey not in list(new.keys())}
+#     else:
+    update = OrderedDict()
 
-    update = {**new, **update}
+    for ikey in source.keys():
+        if ikey not in new.keys():
+            update[ikey] = source[ikey]
+        else:
+            update[ikey] = new[ikey]
+                
+        # update = {f"{ikey}": 1 for ikey in source.keys() if ikey not in list(new.keys())}
+
+    # update = {**new, **update}
 
     msg = f"Dict keys not same...: \n{source.keys()}\n{update.keys()}"
 
@@ -106,3 +158,70 @@ def get_slices(idx: int, da_size: Dict[str, int], patches: Dict[str, int], strid
 
 def list_product(items: List[int]) -> int:
     return reduce((lambda x, y: x * y), [1] + items)
+
+
+def reconstruct_from_items(
+    items: Iterable, 
+    dims_label: Iterable[str], 
+    xrda_batcher,
+    weight=None
+):
+    msg = f"Length of dim labels does not match length of non-batch dims."
+    msg += f"\nDims Label: {dims_label} \nShape: {items[0].shape}"
+    assert len(dims_label) == len(items[0].shape), msg
+    
+    coords = xrda_batcher.get_coords()
+    
+    # check for subset of coordinate arrays
+    coords_labels = list(coords[0].dims.keys())
+    check_lists_subset(coords_labels, dims_label)
+    items_shape = dict(zip(dims_label, items[0].shape))
+    
+    
+    patch_values = list(xrda_batcher.patches.values())
+    patch_names = list(xrda_batcher.patches.keys())
+    
+    # (maybe) update weight matrix
+    if weight is None:
+        weight = np.ones(patch_values)
+    else:
+        msg = "Weight array is not the same size as patch dims "
+        msg += "or not the same value"
+        msg += f"\nWeight: {list(weight.shape)} | Patches: {patch_values}"
+        assert len(weight.shape) == len(xrda_batcher.patches), msg
+        assert any(list(ishape == ivalue for ishape, ivalue in zip(weight.shape, patch_values))), msg
+                    
+    w = xr.DataArray(weight, dims=patch_names)
+    
+    # create data arrays from (maybe) coords
+    das = [
+        xr.DataArray(it, dims=dims_label, coords=co.coords)
+        for it, co in zip(items, coords)
+          ]
+    
+    msg = "New Data Array is not the same size as items"
+    msg += "or not the same value"
+    msg += f"\n{das[0].shape} | Items: {items_shape}"
+    assert len(das[0].shape) == len(items_shape), msg
+    assert set(das[0].shape) == set(items_shape.values()), msg
+    
+    # get new shape from 
+    new_shape = {
+        idim: xrda_batcher.da[idim].shape[0] if idim in coords_labels
+        else items[0].shape[i]
+        for i, idim in enumerate(dims_label) 
+    }
+    
+    rec_da = xr.DataArray(
+        np.zeros([*new_shape.values()]),
+        dims=dims_label,
+        coords={d: xrda_batcher.da[d] for d in xrda_batcher.patches}
+    )
+    count_da = xr.zeros_like(rec_da)
+    
+    for ida in tqdm(das):
+        icoord = ida.coords
+        rec_da.loc[ida.coords] = rec_da.sel(ida.coords) + ida * w
+        count_da.loc[ida.coords] = count_da.sel(ida.coords) + w
+        
+    return rec_da / count_da
