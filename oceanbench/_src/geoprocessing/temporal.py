cb46f924a5d86598219ecacba1fbc906b5a1e19c
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-05-03 12:17:18 +0200

    Updated documentation.

--- oceanbench/_src/geoprocessing/temporal.py
+++ oceanbench/_src/geoprocessing/temporal.py
@@ -2,42 +2,45 @@ from typing import Optional, Union
 import numpy as np
 import xarray as xr
 import pandas as pd
+import pint_xarray
 
 
 def time_rescale(
     ds: xr.Dataset,
-    freq_dt: int=1,
-    freq_unit: str="D",
-    t0: Optional[Union[str, np.datetime64]]=None
+    freq_dt: int = 1,
+    freq_unit: str = "seconds",
+    t0: Optional[Union[str, np.datetime64]] = None,
 ) -> xr.Dataset:
     """Rescales time dimensions of np.datetim64 to an output frequency.
-    
+
     t' = (t - t_0) / dt
-    
+
     Args:
         ds (xr.Dataset): the xr.Dataset with a time dimensions
         freq_dt (int): the frequency of the temporal coordinate
         freq_unit (str): the unit for the time frequency parameter
-        t0 (datetime64, str): the starting point. Optional. If none, assumes the 
+        t0 (datetime64, str): the starting point. Optional. If none, assumes the
             minimum value of the time coordinate
-    
+
     Returns:
-        ds (xr.Dataset): the xr.Dataset with the rescaled time dimensions in the 
+        ds (xr.Dataset): the xr.Dataset with the rescaled time dimensions in the
             freq_unit.
     """
-    
+
     ds = ds.copy()
-    
+
     if t0 is None:
         t0 = ds["time"].min()
-    
+
     if isinstance(t0, str):
         t0 = np.datetime64(t0)
-    
-    ds["time"] = ((ds["time"] - t0 ) / np.timedelta64(freq_dt, freq_unit)).astype(np.float32)
-    
-    ds["time"].attrs["units"] = freq_unit.lower()
-    
+
+    ds["time"] = ((ds["time"] - t0) / pd.to_timedelta(freq_dt, unit=freq_unit)).astype(
+        np.float32
+    )
+
+    ds = ds.pint.quantify({"time": freq_unit})
+
     return ds
 
 
@@ -48,27 +51,27 @@ def time_rescale(
 #     freq_unit: str="D",
 # ) -> xr.Dataset:
 #     """UnRescales time dimensions of an output frequency to a np.datetim64
-#     
+#
 #     t' = t * dt + t_0
-#     
+#
 #     Args:
 #         ds (xr.Dataset): the xr.Dataset with a time dimensions
 #         t0 (datetime64, str): the starting point.
 #         freq_dt (int): the frequency of the temporal coordinate
 #         freq_unit (str): the unit for the time frequency parameter
-#     
+#
 #     Returns:
-#         ds (xr.Dataset): the xr.Dataset with the rescaled time dimensions in the 
+#         ds (xr.Dataset): the xr.Dataset with the rescaled time dimensions in the
 #             np.datetime64 units.
 #     """
-#     
+#
 #     ds = ds.copy()
-#     
+#
 #     if isinstance(t0, str):
 #         t0 = np.datetime64(t0)
-#     
-#     ds["time"] = ds["time"].dt * np.timedelta64(freq_dt, freq_unit) + t0 
-#     
+#
+#     ds["time"] = ds["time"].dt * np.timedelta64(freq_dt, freq_unit) + t0
+#
 #     ds["time"].attrs["units"] = "ns"
-#     
+#
 #     return ds
